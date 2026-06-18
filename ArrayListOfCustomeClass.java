229e01b70fe93ff8952f5a03d7f88a59542ba85c
Anticoder03 <ap5381545@gmail.com>
2026-06-18 10:31:56 +0530

    2d Student Object ArrayList

--- ArrayListOfCustomeClass.java
+++ ArrayListOfCustomeClass.java
@@ -8,8 +8,23 @@ public class ArrayListOfCustomeClass {
 		fy.add(new Student(2,22,"Raj"));
 		fy.add(new Student(3,23,"Sunny"));
 		
-		System.out.println(fy.get(0).toString());
 		System.out.println(fy);
+		
+		ArrayList<Student> sy = new ArrayList();
+		
+		sy.add(new Student(101,21,"Raja"));
+		sy.add(new Student(102,22,"Riya"));
+		sy.add(new Student(103,23,"Priya"));
+		
+		System.out.println(sy);
+		
+		
+//		2D Array 
+		ArrayList<ArrayList<Student>> bca = new ArrayList();
+		bca.add(fy);
+		bca.add(sy);
+		
+		System.out.println(bca);
 		
 		
 	}
